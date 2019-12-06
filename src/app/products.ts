1ca0b38ff66cfe33e0ca71be317689d0dcfd6819
priyatj <pt437@drexel.edu>
2019-12-05 19:12:27 -0500

    front end changes

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,32 +1,38 @@
 export const products = [
   {
-    name: 'Phone Pro Max',
+    name: 'iPhone Pro Max',
     price: 999,
+    imageLink: 'https://www.att.com/catalog/en/idse/Apple/Apple%20iPhone%2011%20Pro%20Max/Gold-hero-zoom.png',
     description: 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro.'
   },
   {
-    name: 'Phone Pro',
+    name: 'iPhone Pro',
     price: 899,
+    imageLink: 'https://cdn.macrumors.com/article-new/2019/09/iphone11prolineup.jpg',
     description: 'Meet the first triple‑camera system to combine cutting‑edge technology with the legendary simplicity of iPhone. Capture up to four times more scene. Get beautiful images in drastically lower light. Shoot the highest‑quality video in a smartphone — then edit with the same tools you love for photos. You’ve never shot with anything like it'
   },
   {
-    name: 'Phone XL',
+    name: 'iPhone XS',
     price: 799,
+    imageLink: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6009/6009655cv13d.jpg',
+    description: 'All-screen OLED multi-touch display- 2436-by-1125-pixel resolution.Embedded M11 motion coprocessor.4K video recording - 7MP camera Front True depth camera.Bluetooth - Wi-Fi - GPS - Audio playback - video playback - splash, water and dust resistant.'
+  },
+  {
+    name: 'iPhone X',
+    price: 699,
+    imageLink:'https://res.cloudinary.com/spectrumobile/image/fetch/e_trim/w_360,h_480,c_fit,f_auto,q_auto,fl_lossy/https%3A%2F%2Fres.cloudinary.com%2Fspectrumobile%2Fimage%2Fupload%2Fv1527810361%2FSpecturm%2520Mobile%2520Buy%2520Flow%2FApple%2FiPhone%2520X%2FMQA52LLA_HERO.png',
     description: 'A new dual‑camera system captures more of what you see and love. The fastest chip ever in a smartphone and all‑day battery life let you do more and charge less. And the highest‑quality video in a smartphone, so your memories look better than ever.'
   },
   {
-    name: 'Phone L',
+    name: 'iPhone 8',
     price: 699,
-    description: 'A new dual‑camera system captures more of what you see and love. The fastest chip ever in a smartphone and all‑day battery life let you do more and charge less. And the highest‑quality video in a smartphone, so your memories look better than ever.'
+    imageLink: 'https://smarter-phone.co/en/3143-product_blocklayeredcustom/apple-iphone-8-plus.jpg',
+    description: 'A great phone with one of the best camera with good image resolution.It has dual sim option.All-day battery life.Internal storage 32GB and expendable upto 128GB external storage.The STUDIO MINI comes with 4G LTE: 2/5/12/17 to ensure you always have access to worldwide carrier coverage while you are on the go.'
   },
   {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    name: 'Phone Standard',
+    name: 'iPhone Standard',
     price: 299,
-    description: ''
+    imageLink: 'https://www.imore.com/sites/imore.com/files/field/image/2014/03/topic_iphone_5s.png',
+    description: 'Smart phone with reasonable price with good camera option.3000mAh battery and Android 9 Pie.The phone is unlocked to work with all GSM Carriers like AT&T, T-Mobile, Etc. Will NOT work with CDMA Carriers Such as Verizon, Sprint, Boost.'
   }
 ];
